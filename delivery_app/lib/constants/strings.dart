40077641828ec87f674aaf25c5db2dcc8a06e646
elhadjmb <hadjmahmoudbederina@gmail.com>
2021-05-02 14:23:41 +0100

    Added constants package for strings and images references

---
+++ delivery_app/lib/constants/strings.dart
@@ -0,0 +1,7 @@
+class inner {
+  static const sAdmin = "sAdmin";
+  static const client = "client";
+  static const server = "server";
+  static const deliverer = "deliverer";
+  static const guest = "guest";
+}
