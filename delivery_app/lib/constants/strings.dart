e0172ac30218f6b59e695f272a3b540bb8bd8048
elhadjmb <hadjmahmoudbederina@gmail.com>
2021-05-04 01:40:45 +0100

    Added Outer class

--- delivery_app/lib/constants/strings.dart
+++ delivery_app/lib/constants/strings.dart
@@ -1,7 +1,13 @@
 class Inner {
+  /*For strings inside the code, not necessarily to be shown to the user*/
   static const sAdmin = "sAdmin";
   static const client = "client";
   static const server = "server";
   static const deliverer = "deliverer";
   static const guest = "guest";
+}
+
+class Outer {
+  /*For strings shown to the user in views*/
+  static const okButton = "OK";
 }
