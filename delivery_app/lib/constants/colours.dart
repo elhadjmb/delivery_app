858ec66be5e552a7b490aeb5abd60cc99ab2bf3d
elhadjmb <hadjmahmoudbederina@gmail.com>
2021-05-06 01:08:07 +0100

    Updated colors and add more

--- delivery_app/lib/constants/colours.dart
+++ delivery_app/lib/constants/colours.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Colour {
-  static const black = Color(0xff000000);
-  static const white = Color(0xffffffff);
-  static const yellow = Color(0xffeeee00);
-  static const red = Color(0xffee0000);
+  static const black = Color(0xff001B29);
+  static const white = Color(0xffF6F2DF);
+  static const yellow = Color(0xffFCBF49);
+  static const red = Color(0xffD62828);
+  static const orange = Color(0xFFF77F00);
+  static const gray = Color(0xFF8F8F8F);
 }
