df2dc3eb670051835311e08ee1ae8feb7c38fcc4
elhadjmb <hadjmahmoudbederina@gmail.com>
2021-05-06 19:00:22 +0100

    Refactored a typo

--- delivery_app/lib/controllers/LaunchController.dart
+++ delivery_app/lib/controllers/LaunchController.dart
@@ -5,7 +5,7 @@ import 'package:delivery_app/models/User.dart';
 import 'package:delivery_app/views/LoginView.dart';
 //import 'package:provider/provider.dart';
 
-class launchCotroller extends StatelessWidget{
+class LaunchController extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
   /*You can test ani screen here ,i put loginscreen momentarily */
--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -4,6 +4,6 @@ import 'controllers/LaunchController.dart';
 void main() {
   runApp(new MaterialApp(
     //TODO: call a controller (LaunchController) that checks if user is signed in then pass
-    home: launchCotroller(),
+    home: LaunchController(),
   ));
 }
