4a3e30181e637c96d89b702c7fbb8ffe23fafbb2
elhadjmb <hadjmahmoudbederina@gmail.com>
2021-05-06 01:07:31 +0100

    Refactored view naming

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'views/Login.dart';
+import 'views/LoginView.dart';
 
 void main() {
   runApp(new MaterialApp(
+    //TODO: call a controller (LaunchController) that checks if user is signed in then pass
     home: LoginScreen(),
   ));
 }
