b1f2130da0d644d77d07426a034f7777eb1bea02
alibeast8 <touaitiaaliali@gmail.com>
2021-05-06 15:52:10 +0100

    Modified the main home to let it run the launch controller

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'views/LoginView.dart';
+import 'controllers/LaunchController.dart';
 
 void main() {
   runApp(new MaterialApp(
     //TODO: call a controller (LaunchController) that checks if user is signed in then pass
-    home: LoginScreen(),
+    home: launchCotroller(),
   ));
 }
