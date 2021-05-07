b19702872aa53e677b3059e1dd064573ffc7858c
alibeast8 <touaitiaaliali@gmail.com>
2021-05-07 19:43:08 +0100

    Added some methodes and a new class

---
+++ delivery_app/lib/controllers/LoginViewController.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:delivery_app/models/User.dart';
+import 'package:delivery_app/views/LoginView.dart';
+import 'package:provider/provider.dart';
+class LoginViewController{
+ // final AuthService _auth= AuthService();
+
+ /* Future signInAnony() async{
+    dynamic result = await _auth.singInAnon();
+    if (result==null){
+    //display error wigdet with try again
+    } 
+    else{
+  print('signed in');
+  //get the new screeen that takes us to homeview
+  return homeview();
+       } 
+  }*/
+}
--- delivery_app/lib/services/Auth.dart
+++ delivery_app/lib/services/Auth.dart
@@ -2,9 +2,21 @@
 import 'package:delivery_app/models/UserSignInData.dart';
 import 'package:delivery_app/services/Database.dart';
 class authService{
-  //final FirebaseAuth _auth = FirebaseAuth.instance;
+//final FirebaseAuth _auth = FirebaseAuth.instance;
 
+//create usersign obj based on firebaseuser
+/*UserSignInData _userfromfirebaseuser(FirebaseUser user){
+ return user != null ? UserSignInData(id: user.id) : null;
 
+}*/
+
+// auth change user stream
+/*Stream<UserSignInData> get user {
+    return _auth.onAuthStateChanged
+      //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+      .map(_userFromFirebaseUser);
+     }
+*/
   //sign in anon
   Future singInAnon() async{
     try {
