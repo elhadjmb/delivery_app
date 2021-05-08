9ec12587ce67ae2b8dae990cd2da549e8efa62fb
BghAek <abdelkaderbabaghayou@gmail.com>
2021-05-08 15:22:33 +0100

    added pizza image

--- delivery_app/lib/constants/images.dart
+++ delivery_app/lib/constants/images.dart
@@ -1,4 +1,5 @@
 class Images {
   static const LoginBackground = "assets/images/LoginBackground.jpg";
   static const burger = "assets/images/burger.png";
+  static const pizza = "assets/images/pizza.png";
 }
