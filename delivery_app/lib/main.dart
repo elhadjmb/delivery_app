e6636c5ae4ccc3fb7dc3bd624a049f2eeb0c5e66
alibeast8 <touaitiaaliali@gmail.com>
2021-05-10 02:51:25 +0100

    Added firebase auth and cloudirestore packages

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -5,13 +5,14 @@ void main() {
   runApp(new MaterialApp(
     home: LaunchController(),
     /*
-    StreamProvider<User>.value(
-      value: AuthService().user,
-      initialData: null,
-      child: new MaterialApp(
-      home: LaunchController(),
-    ),
-  )
+    switch to provider: ^3.1.0 if its not working
+   StreamProvider<UserSignInData>.value(
+        value: authService().user,
+        initialData: UserSignInData(id: '', phone: ''),
+        child: new MaterialApp(
+        home: LaunchController(),
+      ),
+    )
      */
   ));
 }
