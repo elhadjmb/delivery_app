234657c7e34de1b3dad7f40adabfbbae1ad8b231
alibeast8 <touaitiaaliali@gmail.com>
2021-05-22 17:11:04 +0100

    changed the white color to 'ghost white'

--- delivery_app/lib/constants/colours.dart
+++ delivery_app/lib/constants/colours.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Colour {
   static const black = Color(0xff001B29);
-  static const white = Color(0xffF6F2DF);
+  static const white = Color(0xffF4F4F9);
   static const yellow = Color(0xffFCBF49);
   static const red = Color(0xffD62828);
   static const orange = Color(0xFFF77F00);
