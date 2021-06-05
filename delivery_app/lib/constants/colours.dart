9194dba26208d26340eb2aa3ad41ecb51f19bf63
BghAek <abdelkaderbabaghayou@gmail.com>
2021-06-06 00:23:28 +0100

    added a new color

--- delivery_app/lib/constants/colours.dart
+++ delivery_app/lib/constants/colours.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Colour {
+  static const purple = Color.fromRGBO(51, 0, 51, 99.0);
   static const black = Color(0xff001B29);
   static const white = Color(0xffF4F4F9);
   static const yellow = Color(0xffFCBF49);
