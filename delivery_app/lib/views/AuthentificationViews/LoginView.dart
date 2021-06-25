c83ae7cd674bdc5d275e73b75b98fb7a29e32c26
BghAek <abdelkaderbabaghayou@gmail.com>
2021-06-25 17:25:26 +0100

    just called restaurant footer

--- delivery_app/lib/views/AuthentificationViews/LoginView.dart
+++ delivery_app/lib/views/AuthentificationViews/LoginView.dart
@@ -133,7 +133,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => View(),
+                              builder: (context) => Footer(),
                             ),
                           );
                         },
