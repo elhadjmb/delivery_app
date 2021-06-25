123348ed16a7a48a10aba809d46e5be2d7460646
BghAek <abdelkaderbabaghayou@gmail.com>
2021-06-25 17:08:19 +0100

    just changed in data for tests

--- delivery_app/lib/testdata/data.dart
+++ delivery_app/lib/testdata/data.dart
@@ -3,61 +3,71 @@ import 'package:delivery_app/testmodels/ingredient.dart';
 import 'package:delivery_app/testmodels/order.dart';
 import 'package:delivery_app/testmodels/restaurant.dart';
 import 'package:delivery_app/testmodels/user.dart';
-//Ingredient
-final _salade =
-     Ingredient(name: "salade",isCheck: true);
-final _tomato =
-     Ingredient(name: "tomato",isCheck: true);
-final _chicken =
-     Ingredient(name: "chicken",isCheck: true);
-final _chawerma =
-     Ingredient(name: "chawerma",isCheck: true);
-final _onion =
-     Ingredient(name: "onion",isCheck: true);
-final _mayo =
-     Ingredient(name: "mayo",isCheck: true);                         
-final _sauce =
-     Ingredient(name: "sauce",isCheck: true);
-final _herissa =
-     Ingredient(name: "herissa",isCheck: true);
-final _salamon =
-     Ingredient(name: "salmon",isCheck: true);     
-final _meat =
-     Ingredient(name: "meat",isCheck: true);
-final _ketchup =
-     Ingredient(name: "ketchup",isCheck: false);     
-final _tortiglioni =
-     Ingredient(name: "tortiglioni",isCheck: false);  
-final _flour =
-     Ingredient(name: "flour",isCheck: true);  
-final _milk =
-     Ingredient(name: "milk",isCheck: true);  
 
+//Ingredient
+final _salade = Ingredient(name: "salade", isCheck: true);
+final _tomato = Ingredient(name: "tomato", isCheck: true);
+final _chicken = Ingredient(name: "chicken", isCheck: true);
+final _chawerma = Ingredient(name: "chawerma", isCheck: true);
+final _onion = Ingredient(name: "onion", isCheck: true);
+final _mayo = Ingredient(name: "mayo", isCheck: true);
+final _sauce = Ingredient(name: "sauce", isCheck: true);
+final _herissa = Ingredient(name: "herissa", isCheck: true);
+final _salamon = Ingredient(name: "salmon", isCheck: true);
+final _meat = Ingredient(name: "meat", isCheck: true);
+final _ketchup = Ingredient(name: "ketchup", isCheck: false);
+final _tortiglioni = Ingredient(name: "tortiglioni", isCheck: false);
+final _flour = Ingredient(name: "flour", isCheck: true);
+final _milk = Ingredient(name: "milk", isCheck: true);
 
 // Food
-final _burrito =
-    Food(imageUrl: 'assets/testimages/burrito.jpg', name: 'Burrito', price: 8.99, ingredient: [_chicken,_salade,_mayo,_onion]);
-final _steak =
-    Food(imageUrl: 'assets/testimages/steak.jpg', name: 'Steak', price: 17.99, ingredient: [_meat,_sauce,_mayo,_ketchup,_chawerma,_onion]);
-final _pasta =
-    Food(imageUrl: 'assets/testimages/pasta.jpg', name: 'Pasta', price: 14.99, ingredient: [_tortiglioni,_sauce,_ketchup]);
-final _ramen =
-    Food(imageUrl: 'assets/testimages/ramen.jpg', name: 'Ramen', price: 13.99, ingredient: [_sauce,_chawerma,_mayo,_tomato]);
-final _pancakes =
-    Food(imageUrl: 'assets/testimages/pancakes.jpg', name: 'Pancakes', price: 9.99, ingredient: [_flour,_milk]);
-final _burger =
-    Food(imageUrl: 'assets/testimages/burger.jpg', name: 'Burger', price: 14.99, ingredient: [_chicken,_meat,_tomato,_salade,_ketchup,_herissa,_mayo]);
-final _pizza =
-    Food(imageUrl: 'assets/testimages/pizza.jpg', name: 'Pizza', price: 11.99, ingredient: [_meat,_chicken,_tomato,_sauce,_mayo,_herissa,_onion]);
+final _burrito = Food(
+    imageUrl: 'assets/testimages/burrito.jpg',
+    name: 'Burrito',
+    price: 8.99,
+    ingredient: [_chicken, _salade, _mayo, _onion]);
+final _steak = Food(
+    imageUrl: 'assets/testimages/steak.jpg',
+    name: 'Steak',
+    price: 17.99,
+    ingredient: [_meat, _sauce, _mayo, _ketchup, _chawerma, _onion]);
+final _pasta = Food(
+    imageUrl: 'assets/testimages/pasta.jpg',
+    name: 'Pasta',
+    price: 14.99,
+    ingredient: [_tortiglioni, _sauce, _ketchup]);
+final _ramen = Food(
+    imageUrl: 'assets/testimages/ramen.jpg',
+    name: 'Ramen',
+    price: 13.99,
+    ingredient: [_sauce, _chawerma, _mayo, _tomato]);
+final _pancakes = Food(
+    imageUrl: 'assets/testimages/pancakes.jpg',
+    name: 'Pancakes',
+    price: 9.99,
+    ingredient: [_flour, _milk]);
+final _burger = Food(
+    imageUrl: 'assets/testimages/burger.jpg',
+    name: 'Burger',
+    price: 14.99,
+    ingredient: [_chicken, _meat, _tomato, _salade, _ketchup, _herissa, _mayo]);
+final _pizza = Food(
+    imageUrl: 'assets/testimages/pizza.jpg',
+    name: 'Pizza',
+    price: 11.99,
+    ingredient: [_meat, _chicken, _tomato, _sauce, _mayo, _herissa, _onion]);
 final _salmon = Food(
-    imageUrl: 'assets/testimages/salmon.jpg', name: 'Salmon Salad', price: 12.99, ingredient: [_salamon,_salade,_sauce]);
+    imageUrl: 'assets/testimages/salmon.jpg',
+    name: 'Salmon Salad',
+    price: 12.99,
+    ingredient: [_salamon, _salade, _sauce]);
 
 // Restaurants
 final _restaurant0 = Restaurant(
   imageUrl: 'assets/testimages/restaurant0.jpg',
   name: 'Restaurant 0',
   address: '200 Main St, New York, NY',
-  rating: 5,
+  rating: 4,
   menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant1 = Restaurant(
