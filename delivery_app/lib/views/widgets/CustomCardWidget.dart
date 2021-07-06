ab5d50660949cbfbf539c048b89b93c6d7ab9720
elhadjmb <hadjmahmoudbederina@gmail.com>
2021-07-06 15:20:50 +0100

    Reprogrammed the Card widget to fit the profile ui need

---
+++ delivery_app/lib/views/widgets/CustomCardWidget.dart
@@ -0,0 +1,43 @@
+import 'package:delivery_app/constants/colours.dart';
+import 'package:flutter/material.dart';
+
+class CustomCardWidget extends StatelessWidget {
+  final String label;
+  final String text;
+
+  CustomCardWidget({required this.text, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+      color: Colour.white,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Center(
+              child: Text(
+                this.label,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colour.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                this.text,
+                style: TextStyle(
+                  color: Colour.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
